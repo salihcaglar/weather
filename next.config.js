5f5fc87362bfcf0301245b74e590b71189926bac
Salih Çağlar <salih_ts@hotmail.com>
2023-03-02 17:28:46 +0300

    first commit created

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+        domains: ["images.unsplash.com", "openweathermap.org"]
+    },
+  env: {
+    DB_KEY: process.env.DB_KEY
+  }
 }
 
 module.exports = nextConfig
